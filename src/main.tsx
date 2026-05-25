a381fd1c08fef17397ecadbf5a8b85a7a04dc7a4
majo <mariajoseaguilar188@gmail.com>
2026-05-24 19:53:17 -0500

    mantener modo oscuro al cargar

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+// Aplicar modo oscuro al cargar si estaba activado
+if (localStorage.getItem("darkMode") === "true") {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
